4cb3dc2e5755d1df1859b57a64635474d7d3ba33
Joeven <marketing@rapidcleanillawarra.com.au>
2026-02-11 11:12:44 +0800

    feat: add RollerSpec.svelte component to generate dynamic SVG diagrams for roller specifications, mapping inputs to variables and rendering conditional slot types.

--- src/routes/drawings/draw/components/RollerSpec.svelte
+++ src/routes/drawings/draw/components/RollerSpec.svelte
@@ -42,7 +42,7 @@
     let faceWidth = $derived(getVal("face_width", 0));
     let slotWidth = $derived(getVal("slot_width", 0));
     let backToBack = $derived(getVal("back_to_back", 0));
-    let typeOfSlot = $derived(getVal("type_of_slot", ""));
+    let typeOfSlot = $derived(String(getVal("type_of_slot", "4")));
     let shaftOverall = $derived(getVal("shaft_overall", 0));
     let shaftDiameter = $derived(getVal("shaft_diameter", 0));
     let rollerDiameter = $derived(getVal("roller_diameter", 0));
@@ -147,27 +147,42 @@
             stroke-width="1"
         />
 
-        <!-- type_of_slot rectangles outside parallel lines -->
+        <!-- Type-dependent slots -->
         <!-- Left side -->
-        <rect
-            x="25"
-            y="95"
-            width="20"
-            height="20"
-            fill="none"
-            stroke="#1e1b4b"
-            stroke-width="1"
-        />
+        {#if typeOfSlot === "2"}
+            <path
+                d="m 1.3946827,0.24696316 6.6965304,-0.08134 c 1.817686,-0.119544 0.03757,2.63928104 2.2366689,3.00960104 l 7.099199,-0.02236 c 2.035075,0.0017 0.607359,-2.93530304 2.426854,-2.90588004 l 9.970693,-0.04648 L 29.838118,19.76873 1.3677507,19.83845 C 0.44077471,19.839324 0.22918071,19.340052 0.23593971,18.688059 l -0.06738,-17.4068928 c -0.09179,-0.62045204 0.749669,-0.98153804 1.22610599,-1.03418804 z"
+                transform="translate(15.2, 95)"
+                fill="none"
+                stroke="#1e1b4b"
+                stroke-width="1"
+            />
+        {:else}
+            <path
+                d="M 45 95 L 45 115 L 28 115 A 3 3 0 0 1 25 112 L 25 98 A 3 3 0 0 1 28 95 Z"
+                fill="none"
+                stroke="#1e1b4b"
+                stroke-width="1"
+            />
+        {/if}
+
         <!-- Right side -->
-        <rect
-            x="252"
-            y="95"
-            width="20"
-            height="20"
-            fill="none"
-            stroke="#1e1b4b"
-            stroke-width="1"
-        />
+        {#if typeOfSlot === "2"}
+            <path
+                d="m 1.3946827,0.24696316 6.6965304,-0.08134 c 1.817686,-0.119544 0.03757,2.63928104 2.2366689,3.00960104 l 7.099199,-0.02236 c 2.035075,0.0017 0.607359,-2.93530304 2.426854,-2.90588004 l 9.970693,-0.04648 L 29.838118,19.76873 1.3677507,19.83845 C 0.44077471,19.839324 0.22918071,19.340052 0.23593971,18.688059 l -0.06738,-17.4068928 c -0.09179,-0.62045204 0.749669,-0.98153804 1.22610599,-1.03418804 z"
+                transform="translate(281.8, 95) scale(-1, 1)"
+                fill="none"
+                stroke="#1e1b4b"
+                stroke-width="1"
+            />
+        {:else}
+            <path
+                d="M 252 95 L 252 115 L 269 115 A 3 3 0 0 0 272 112 L 272 98 A 3 3 0 0 0 269 95 Z"
+                fill="none"
+                stroke="#1e1b4b"
+                stroke-width="1"
+            />
+        {/if}
     </svg>
 </div>
 
